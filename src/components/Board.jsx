03eec6c655f12ee7e09f1684e56aff8239251bbf
hayatul-islam <hayatulemon340.com>
2024-01-10 14:50:44 +0600

    primary game functionality done

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,19 +1,43 @@
-export default function Board({ squires }) {
-  console.log(squires);
-  const handClick = (value) => {
-    console.log(value);
+import { calculateWinner } from "../utils/calculateWinner";
+
+export default function Board({ squires, setHistory, setIsNext, isNext }) {
+  let status;
+  const winner = calculateWinner(squires);
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next Player is ${isNext ? "O" : "X"}`;
+  }
+
+  const handClick = (i) => {
+    if (squires[i] || calculateWinner(squires)) {
+      return;
+    }
+
+    let nextSquares = [...squires];
+    if (isNext) {
+      nextSquares[i] = "O";
+    } else {
+      nextSquares[i] = "X";
+    }
+    setHistory(nextSquares);
+    setIsNext(!isNext);
   };
+
   return (
-    <div className="grid grid-cols-3">
-      {squires.map((value, i) => (
-        <button
-          key={i}
-          onClick={() => handClick(i)}
-          className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
-        >
-          {value}
-        </button>
-      ))}
+    <div>
+      <p>{status}</p>
+      <div className="grid grid-cols-3">
+        {squires?.map((value, i) => (
+          <button
+            key={i}
+            onClick={() => handClick(i)}
+            className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
+          >
+            {value}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,15 +1,23 @@
 import { useState } from "react";
 import Board from "./Board";
+import History from "./History";
 
 export default function Game() {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [history, setHistory] = useState(Array(9).fill(null));
+  const [isNext, setIsNext] = useState(false);
+
   return (
     <div className="flex justify-center p-4">
       <div className="mr-16">
-        <Board squires={history[history.length - 1]} />
+        <Board
+          squires={history}
+          setHistory={setHistory}
+          setIsNext={setIsNext}
+          isNext={isNext}
+        />
       </div>
       <div>
-        <ol className="border border-gray-400 p-1 text-lg"></ol>
+        <History />
       </div>
     </div>
   );
